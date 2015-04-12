f8c2a4b0e0a898693222e7190eb0e8f13434a79e
Rémy Kalou <remy.kaloustian@gmail.com>
2015-04-12 10:31:10 +0200

    Comment Lines
    Add comment Lines for the internship

--- Mover.cs
+++ Mover.cs
@@ -1,17 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Mover : MonoBehaviour {
+public class Mover : MonoBehaviour
+{
 
-	public float speed;
-	// Use this for initialization
-	void Start () 
-	{
-		rigidbody.velocity = transform.forward * speed;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-}
+    public float speed;//Dans l'éditeur
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody.velocity = transform.forward * speed; //Va tout droit grâce au transfrom.forward
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}//Mover
--- PlayerController.cs
+++ PlayerController.cs
@@ -1,49 +1,52 @@
 ﻿using UnityEngine;
 using System.Collections;
-[System.Serializable] 
+[System.Serializable] //Classe visible dans l'inspecteur
 public class Boundary
 {
-	public float xMin, xMax, zMin, zMax;
+    public float xMin, xMax, zMin, zMax;
 
 }//class Boundary
 
 public class PlayerController : MonoBehaviour
 {
-	public float m_speed;
-	public float tilt;
-	public Boundary m_boundary;
+    public float m_speed; //A changer dans l'éditeur
+    public float tilt;
+    public Boundary m_boundary;
 
-	public GameObject shot;
-	public Transform shotSpawn;
-	public float fireRate;
-	private float nextFire;
+    public GameObject shot; //Linké à travers l'éditeur
+    public Transform shotSpawn;
+    public float fireRate;
+    private float nextFire;
 
-	void Update()
-	{//Input.GetButton ("Fire1")
-		if (Input.GetKeyDown ("space") && Time.time > nextFire) 
-		{
-			nextFire = Time.time + fireRate;
-			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
-			audio.Play();
-		}
+    void Update() //appelée à chaque frame
+    {//Input.GetButton ("Fire1")
+        if (Input.GetKeyDown("space") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            audio.Play();
+        }
 
-	}
+    }
 
-	void FixedUpdate()
-	{	
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis ("Vertical");
+    void FixedUpdate() // Appelée automatiquement par Unity juste avant chaque thick physique
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //Déplacement
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.velocity = movement * m_speed;//Change la position du vaisseau
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-		rigidbody.velocity = movement * m_speed;
-		rigidbody.position = new Vector3
-			(
-				Mathf.Clamp(rigidbody.position.x, m_boundary.xMin, m_boundary.xMax),
-				0.0f,
-				Mathf.Clamp(rigidbody.position.z, m_boundary.zMin, m_boundary.zMax) 
-			);
+        //Empêche de sorttir de la map
+        rigidbody.position = new Vector3
+            (//Mathf contient des fonctions mathématiques super cools
+            //Clamp met la valeur entre un minimum et un maximum
+                Mathf.Clamp(rigidbody.position.x, m_boundary.xMin, m_boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rigidbody.position.z, m_boundary.zMin, m_boundary.zMax)
+            );
 
-		rigidbody.rotation = Quaternion.Euler (0.0f, 0.0f, rigidbody.velocity.x * -tilt);
-	}//FixedUpdate()
+        rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, rigidbody.velocity.x * -tilt);
+    }//FixedUpdate()
 
 }//class PlayerController
